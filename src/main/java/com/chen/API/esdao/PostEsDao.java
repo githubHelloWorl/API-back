b7c63d3ee8423e134e543000fdaf753014e55d23
cmd <3417372950@qq.com>
2024-07-25 14:38:49 +0800

    init

---
+++ src/main/java/com/chen/API/esdao/PostEsDao.java
@@ -0,0 +1,13 @@
+package com.chen.API.esdao;
+
+import com.chen.API.model.dto.post.PostEsDTO;
+import java.util.List;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+/**
+ * 帖子 ES 操作
+ */
+public interface PostEsDao extends ElasticsearchRepository<PostEsDTO, Long> {
+
+    List<PostEsDTO> findByUserId(Long userId);
+}
---
+++ src/main/java/com/chen/API/service/PostThumbService.java
@@ -0,0 +1,29 @@
+package com.chen.API.service;
+
+import com.chen.API.model.entity.PostThumb;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.chen.API.model.entity.User;
+
+/**
+ * 帖子点赞服务
+ */
+public interface PostThumbService extends IService<PostThumb> {
+
+    /**
+     * 点赞
+     *
+     * @param postId
+     * @param loginUser
+     * @return
+     */
+    int doPostThumb(long postId, User loginUser);
+
+    /**
+     * 帖子点赞（内部服务）
+     *
+     * @param userId
+     * @param postId
+     * @return
+     */
+    int doPostThumbInner(long userId, long postId);
+}
